1b7671bded7f9612c1a06e446d0f95f60e8f3724
Pawlicka Maule <anna.maulle@gmail.com>
2016-07-05 17:42:21 -0700

    updating

---
+++ report.py
@@ -0,0 +1,53 @@
+#-*- coding: utf-8 -*-
+
+#Anna Paula Pawlicka Maule 
+
+import sys
+import socket
+import ssl
+import httplib
+from github import Github
+
+def load_orgs():
+	 """ read in orgs.txt (one org per line) in to a list
+    main calls report_orgs() """
+
+
+
+def report_orgs(): 
+	""" or each org in orgs_list print "total number or repos is X" followed by a list of names (hint)"""
+
+#user = raw_input("user name: ")
+#str(user)
+#password=raw_input("password: ")
+#str(password)
+# First create a Github instance:
+user = raw_input("user name: ")
+str(user)
+g = Github()
+
+
+if sys.argv[1] == "all" :
+
+	# getting the user's organizations :
+	for org in g.get_user(user).get_orgs():
+	    print "\nOrganization Name:  " + org.name 
+	    print "# Private repos : " + str(org.owned_private_repos)
+	    print "# Public repos : " + str(org.public_repos) + "\n"
+
+
+	# listing the organization's repositories 
+	    for repo in org.get_repos():
+	    	print repo.name 
+    
+else: 
+	# getting the user's organizations :
+	for org in g.get_user("mattben").get_orgs():
+	    print "\nOrganization Name:  " + org.name 
+	    print "# Private repos : " + str(org.owned_private_repos)
+	    print "# Public repos : " + str(org.public_repos) + "\n"
+
+
+
+
+   
